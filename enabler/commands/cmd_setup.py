c51d97a81bca4111d1d6a0d8b563673536a39f8a
taleksovska <taleksovska@ibasis.net>
2024-03-06 13:14:58 +0100

    WIP - Code optimization

--- enabler/commands/cmd_setup.py
+++ enabler/commands/cmd_setup.py
@@ -31,31 +31,31 @@ def cli(ctx, kube_context_cli):
 def init(ctx, kube_context_cli):
     """Download binaries for all dependencies"""
 
-    # Check if kubectl exists in any directory
-    kubectl_location = shutil.which('kubectl')
-    
     # Figure out what kind of OS are we on
     ostype = os.uname().sysname.lower()
 
-    if kubectl_location:
-        logger.info(f'kubectl found at: {kubectl_location}. Continuing with other dependencies...')
-    else:
-        logger.info('kubectl not found on the system. Attempting to download...')
-        
-        with click_spinner.spinner():
-            # Download kubectl and make executable
+    # URLs for dependencies
+    kubectl_url = 'https://storage.googleapis.com/kubernetes-release/release/v1.17.3/bin/{}/amd64/kubectl'.format(ostype)  # noqa
+    helm_url = 'https://get.helm.sh/helm-v3.1.2-{}-amd64.tar.gz'.format(ostype)
+    istioctl_url = 'https://github.com/istio/istio/releases/download/1.5.1/istioctl-1.5.1-{}.tar.gz'.format(ostype)  # noqa
+    kind_url = 'https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-{}-amd64'.format(ostype)  # noqa
+    skaffold_url = 'https://storage.googleapis.com/skaffold/releases/latest/skaffold-{}-amd64'.format(ostype)  # noqa
+
+    with click_spinner.spinner():
+        # Download kubectl and make executable
+        kubectl_location = shutil.which('kubectl')
+        if kubectl_location:
+            logger.info(f'kubectl found at: {kubectl_location}')
+        else:
+            logger.info('kubectl not found. Attempting to download...')
             logger.info('Downloading kubectl...')
-            kubectl_url = 'https://storage.googleapis.com/kubernetes-release/release/v1.17.3/bin/{}/amd64/kubectl'.format(ostype)  # noqa
             urllib.request.urlretrieve(kubectl_url, 'bin/kubectl')
             st = os.stat('bin/kubectl')
             os.chmod('bin/kubectl', st.st_mode | stat.S_IEXEC)
             logger.info('kubectl downloaded!')
 
-    with click_spinner.spinner():
         # Download helm
         logger.info('Downloading helm...')
-        helm_url = 'https://get.helm.sh/helm-v3.1.2-{}-amd64.tar.gz'.format(ostype)
-        os.makedirs('bin', exist_ok=True)
         urllib.request.urlretrieve(helm_url, 'bin/helm.tar.gz')
         tar = tarfile.open('bin/helm.tar.gz', 'r:gz')
         for member in tar.getmembers():
@@ -66,10 +66,8 @@ def init(ctx, kube_context_cli):
         os.remove('bin/helm.tar.gz')
         logger.info('helm downloaded!')
 
-
         # Download istioctl
         logger.info('Downloading istioctl...')
-        istioctl_url = 'https://github.com/istio/istio/releases/download/1.5.1/istioctl-1.5.1-{}.tar.gz'.format(ostype)  # noqa
         urllib.request.urlretrieve(istioctl_url, 'bin/istioctl.tar.gz')
         tar = tarfile.open('bin/istioctl.tar.gz', 'r:gz')
         tar.extract('istioctl', 'bin')
@@ -79,7 +77,6 @@ def init(ctx, kube_context_cli):
 
         # Download kind and make executable
         logger.info('Downloading kind...')
-        kind_url = 'https://github.com/kubernetes-sigs/kind/releases/download/v0.7.0/kind-{}-amd64'.format(ostype)  # noqa
         urllib.request.urlretrieve(kind_url, 'bin/kind')
         st = os.stat('bin/kind')
         os.chmod('bin/kind', st.st_mode | stat.S_IEXEC)
@@ -87,17 +84,16 @@ def init(ctx, kube_context_cli):
 
         # Download skaffold and make executable
         logger.info('Downloading skaffold...')
-        skaffold_url = 'https://storage.googleapis.com/skaffold/releases/latest/skaffold-{}-amd64'.format(ostype)  # noqa
         urllib.request.urlretrieve(skaffold_url, 'bin/skaffold')
         st = os.stat('bin/skaffold')
         os.chmod('bin/skaffold', st.st_mode | stat.S_IEXEC)
         logger.info('skaffold downloaded!\n')
 
-        logger.info('All dependencies downloaded to bin/')
-        logger.info('IMPORTANT: Please add the path to your user profile to ' +
-                    os.getcwd() + '/bin directory at the beginning of your PATH')
-        logger.info('$ echo export PATH=' + os.getcwd() + '/bin:$PATH >> ~/.profile')  # noqa
-        logger.info('$ source ~/.profile')
+    logger.info('All dependencies downloaded to bin/')
+    logger.info('IMPORTANT: Please add the path to your user profile to ' +
+                os.getcwd() + '/bin directory at the beginning of your PATH')
+    logger.info('$ echo export PATH=' + os.getcwd() + '/bin:$PATH >> ~/.profile')  # noqa
+    logger.info('$ source ~/.profile')
 
 
 # Metallb setup
@@ -114,7 +110,7 @@ def init(ctx, kube_context_cli):
               default='4.6.0')
 @click.pass_context
 @pass_environment
-def metallb(ctx, kube_context_cli, kube_context, ip_addresspool,version):
+def metallb(ctx, kube_context_cli, kube_context, ip_addresspool, version):
     """Install and setup metallb on k8s"""
     # Check if metallb is installed
     if ctx.kube_context is not None:
@@ -138,21 +134,19 @@ def metallb(ctx, kube_context_cli, kube_context, ip_addresspool,version):
     except s.CalledProcessError as error:
         logger.info('Metallb not found. Installing...')
         pass
-    
-    #Get repo version
     try:
         metallb_repo_add = s.run(['helm',
-                                'repo',
-                                'add',
-                                'bitnami',
-                                'https://charts.bitnami.com/bitnami'],
-                               capture_output=True, check=True)
+                                  'repo',
+                                  'add',
+                                  'bitnami',
+                                  'https://charts.bitnami.com/bitnami'],
+                                   capture_output=True, check=True)
         logger.info('Downloading metallb version ...')
         logger.debug(metallb_repo_add.stdout.decode('utf-8'))
         metallb_repo_add = s.run(['helm',
-                                'repo',
-                                'update'],
-                               capture_output=True, check=True)
+                                  'repo',
+                                  'update'],
+                                capture_output=True, check=True)
         logger.info('Repo update ...')
         logger.debug(metallb_repo_add.stdout.decode('utf-8'))
 
@@ -164,7 +158,6 @@ def metallb(ctx, kube_context_cli, kube_context, ip_addresspool,version):
     # Get the Subnet of the kind network
     client = docker.from_env()
     networks = client.networks()
-
 
     # Find the network with name 'kind'
     kind_network = None  # Initialize kind_network outside the loop
@@ -185,44 +178,36 @@ def metallb(ctx, kube_context_cli, kube_context, ip_addresspool,version):
     metallb_ips = ips[-10:]
 
     if ip_addresspool is None:
-        ip_addresspool= metallb_ips[0] + ' - ' + metallb_ips[-1]
+        ip_addresspool = metallb_ips[0] + ' - ' + metallb_ips[-1]
     else:
-        #Check if ip address range is in kind network and print out a warning
-        ip_range=ip_addresspool.strip().split('-')
+        # Check if ip address range is in kind network and print out a warning
+        ip_range = ip_addresspool.strip().split('-')
         try:
-            start_ip=ipaddress.IPv4Address(ip_range[0])
-            end_ip=ipaddress.IPv4Address(ip_range[1])
+            start_ip = ipaddress.IPv4Address(ip_range[0])
+            end_ip = ipaddress.IPv4Address(ip_range[1])
         except:
-            logger.error('Incorrect IP address range: '+ ip_addresspool)
+            logger.error('Incorrect IP address range: ' + ip_addresspool)
 
         if start_ip not in ipaddress.IPv4Network(kind_subnet) or end_ip not in ipaddress.IPv4Network(kind_subnet):
             logger.error('Provided IP address range not in kind network. Kind subnet is: ' + kind_subnet)
             raise click.Abort()
 
-    #Check if version is 3.x.x and then use config map to install, else if version 4.x.x use CDR file for installing.
-    #And dynamically set the IP Address range in the compatible .yaml file
-    if version.split('.')[0]=='3':
-        yaml_file_path='enabler/metallb-configmap.yaml'
+    # Check if version is 3.x.x and then use config map to install, else if version 4.x.x use CDR file for installing.
+    # And dynamically set the IP Address range in the compatible .yaml file
+    if version.split('.')[0] == '3':
+        yaml_file_path = 'enabler/metallb-configmap.yaml'
         with open(yaml_file_path, 'r') as yaml_file:
             config = yaml.safe_load(yaml_file)
-        
-        
         modified_string = config['data']['config'][:-32]+ip_addresspool+"\n"
-        
         config['data']['config'] = modified_string 
 
-
         logger.info('Metallb will be configured in Layer 2 mode with the range: ' + ip_addresspool)
-
-        updated_yaml=yaml.dump(config, default_flow_style=False)
-        
+        updated_yaml = yaml.dump(config, default_flow_style=False)        
         with open(yaml_file_path, 'w') as yaml_file:
             yaml_file.write(updated_yaml)
 
-
-
-    elif int(version.split('.')[0])>=4:
-        yaml_file_path='enabler/metallb-crd.yaml'
+    elif int(version.split('.')[0]) >= 4:
+        yaml_file_path = 'enabler/metallb-crd.yaml'
         with open(yaml_file_path, 'r') as yaml_file:
             config = list(yaml.safe_load_all(yaml_file))
 
@@ -235,8 +220,6 @@ def metallb(ctx, kube_context_cli, kube_context, ip_addresspool,version):
             yaml.dump_all(config, yaml_file, default_flow_style=False)
     else:
         logger.info('Incompatible format for Metallb version. Please check official versions ')
-    
-
     ns_exists = s.run(['kubectl',
                        'get',
                        'ns',
@@ -270,27 +253,24 @@ def metallb(ctx, kube_context_cli, kube_context, ip_addresspool,version):
                               'kind-' + kube_context,
                               '--version',
                               version,
-                              'bitnami/metallb' ,                            
+                              'bitnami/metallb',                            
                               '-n',
                               'metallb',
                               '--wait'],
                              capture_output=True, check=True)
+        
         # Apply configuration from CRD file  
-        config_metallb=s.run(['kubectl',
-                                'apply',
-                                '-f',
-                                yaml_file_path],
-                                capture_output=True, check=True)
-
+        config_metallb = s.run(['kubectl',
+                        'apply',
+                        '-f',
+                        yaml_file_path],
+                        capture_output=True, check=True)
 
         logger.info('✓ Metallb installed on cluster.')
         logger.debug(helm_metallb.stdout.decode("utf-8"))
     except s.CalledProcessError as error:
         logger.error('Could not install metallb')
         logger.error(error.stderr.decode('utf-8'))
-
-
-
 
 # Istio setup
 @cli.command('istio', short_help='Setup Istio')
@@ -302,7 +282,7 @@ def metallb(ctx, kube_context_cli, kube_context, ip_addresspool,version):
                 )
 @click.pass_context
 @pass_environment
-def istio(ctx, kube_context_cli,kube_context, monitoring_tools):
+def istio(ctx, kube_context_cli, kube_context, monitoring_tools):
     """Install and setup istio on k8s"""
     if ctx.kube_context is not None:
         kube_context = ctx.kube_context
@@ -327,14 +307,14 @@ def istio(ctx, kube_context_cli,kube_context, monitoring_tools):
     # Install Istio
     logger.info('Installing istio, please wait...')
     with click_spinner.spinner():
-        istio_command=['istioctl',
+        istio_command = ['istioctl',
                         'manifest',
                         'apply',
                         '-y',
                         '--set',
                         'profile=default']
-        if monitoring_tools=='monitoring-tools':
-            monitoring_config=['--set',
+        if monitoring_tools == 'monitoring-tools':
+            monitoring_config = ['--set',
                                    'addonComponents.grafana.enabled=true',
                                    '--set',
                                    'addonComponents.kiali.enabled=true',
@@ -346,10 +326,10 @@ def istio(ctx, kube_context_cli,kube_context, monitoring_tools):
                                    'values.kiali.dashboard.jaegerURL=http://jaeger-query:16686',  # noqa
                                    '--set',
                                    'values.kiali.dashboard.grafanaURL=http://grafana:3000']  # noqa
-            istio_command.extend(monitoring_config)
-            
+        
+        istio_command.extend(monitoring_config)            
         istio_command.append('--context')
-        istio_command.append('kind-'+ kube_context)
+        istio_command.append('kind-' + kube_context)
         istio_command.append('--wait')
         try:
             istio_install = s.run(istio_command,
@@ -360,12 +340,11 @@ def istio(ctx, kube_context_cli,kube_context, monitoring_tools):
             logger.critical('Istio installation failed')
             logger.critical(error.stderr.decode('utf-8'))
             raise click.Abort()
-        if monitoring_tools=='monitoring-tools':
+        if monitoring_tools == 'monitoring-tools':
             try:
-                grafana_virtual_service=s.run(['kubectl','apply','-f','enabler/grafana-vs.yaml'],
+                grafana_virtual_service = s.run(['kubectl', 'apply', '-f', 'enabler/grafana-vs.yaml'],
                                   capture_output=True, check=True)
             except Exception as e:
                 logger.error('Error setting grafana URL')
                 logger.error(str(e))
-            
-                
+        
