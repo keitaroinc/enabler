7f1ae584f54150a967067b37a1209aaacb57a8de
taleksovska <taleksovska@ibasis.net>
2024-05-14 13:21:23 +0200

    Add semantic versioning

--- src/enabler_keitaro_inc/commands/cmd_version.py
+++ src/enabler_keitaro_inc/commands/cmd_version.py
@@ -1,6 +1,7 @@
 from src.enabler_keitaro_inc.enabler import pass_environment, logger
 import pkg_resources
 import click
+from src.enabler_keitaro_inc.type.semver import BasedVersionParamType
 
 # Command to get the current Enabler version
 @click.group('version', short_help='Get current version of Enabler', invoke_without_command=True) # noqa
@@ -10,4 +11,6 @@ def cli(ctx, kube_context_cli):
     """Get current version of Enabler"""
     distribution = pkg_resources.get_distribution("enabler")
     version = distribution.version
-    logger.info("Enabler "+version)
+    # Ensure the version string has three parts (major, minor, patch)
+    formatted_version = BasedVersionParamType().convert(version, None, None)
+    logger.info(f"Enabler {formatted_version}")
--- src/enabler_keitaro_inc/type/semver.py
+++ src/enabler_keitaro_inc/type/semver.py
@@ -8,13 +8,15 @@ class BasedVersionParamType(click.ParamType):
     def convert(self, value, param, ctx):
         try:
             parse(value)
-            return (value)
-        except TypeError:
-            self.fail(
-                '{value!r} is not a valid version, please use semver',
-                param,
-                ctx,
-            )
-        except ValueError:
-            self.fail(f'{value!r} is not a valid version, please use semver',
-                      param, ctx)
+            return value
+        except (TypeError, ValueError):
+            parts = value.split('.')
+            if len(parts) == 2:
+                parts.append('0')  # Default patch version to 0 if not provided
+            elif len(parts) == 3:
+                # Increment patch version for bug fixes
+                parts[2] = str(int(parts[2]) + 1)
+            else:
+                self.fail(f'{value!r} is not a valid version, please use semver', param, ctx) # noqa
+
+            return '.'.join(parts)
